24527b2714b0c66bfafeb7d46606ab0b658217a7
thyago608 <thyagoribeiro608@gmail.com>
2021-09-23 23:34:22 -0400

    Created component Header

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { Header } from '../components/Header';
+import { AppProps } from 'next/app';
+import '../styles/global.scss';
+
+
+function MyApp({Component, pageProps}:AppProps){
+    return (
+        <>
+            <Header/>
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default MyApp;
