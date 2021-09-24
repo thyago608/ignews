71f841ed4a6874771339eaaa9de58b9ce45fb58d
thyago608 <thyagoribeiro608@gmail.com>
2021-09-24 00:48:32 -0400

    Created component SignInButton

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,25 @@
+import { FaGithub} from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export function SignInButton(){
+    const isUserLoggedIn = true;
+   
+    return isUserLoggedIn ? (
+        <button 
+            type="button"
+            className={styles.signInButton}>
+            <FaGithub color="#04d361"/>
+            Thyago Ribeiro
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+    ):(
+        <button 
+            type="button"
+            className={styles.signInButon}
+        >
+            <FaGithub color="#eba417"/>
+            Sign in with Github
+        </button>
+    );
+}
