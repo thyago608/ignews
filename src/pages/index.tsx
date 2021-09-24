cdd4514ce957ec3247ddad94c2dfb26bfc0619a7
thyago608 <thyagoribeiro608@gmail.com>
2021-09-24 11:54:34 -0400

    Created integration stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { SubscribleButton } from '../components/SubscribleButton';
 import styles from './home.module.scss';
 
-export default function Home(){
+export default function Home(props){
+
     return(
         <>
             <Head>
@@ -16,9 +19,21 @@ export default function Home(){
                         Get access to all the publications <br/>
                         <span>for $9.90 month</span> 
                     </p>
+
+                    <SubscribleButton/>
                 </section>
                 <img src="/images/avatar.svg" alt="girl coding"/>
             </main>
         </>
     );
+}
+
+export const getServerSideProps:GetServerSideProps = async () =>{
+
+    return {
+        props:{
+            name: 'Thyago',
+            age: 27
+        }
+    }
 }
