8ae9622b9a6b2399243b6d9d5531120f6be6922d
thyago608 <thyagoribeiro608@gmail.com>
2021-10-05 09:28:19 -0400

    Added user creation in fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { query as q} from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
+
 
 export default NextAuth({
   //Configuração dos providers da aplicação
@@ -10,4 +14,42 @@ export default NextAuth({
       scope:'read:user'  //concede acesso as informações básicas do perfil do usuário
     }),
   ],
+  callbacks:{
+    async signIn(user, account, profile){
+      
+      try{
+
+        const { email } = user;
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          ),
+        );
+
+        return true;
+
+      }catch{
+        return false;
+      }
+    
+    }
+  }
 })
