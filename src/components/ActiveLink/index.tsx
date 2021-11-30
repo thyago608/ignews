8860c58f661c0923ec6c22f2073a5fd374905850
thyago608 <thyagoribeiro608@gmail.com>
2021-11-30 18:47:59 -0400

    Implemented component ActiveLink

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,26 @@
+import { ReactElement, cloneElement } from "react";
+import { useRouter } from "next/router";
+import Link, { LinkProps } from "next/link";
+
+type ActiveLinkProps = LinkProps & {
+  children: ReactElement;
+  activeClassName: string;
+};
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : "";
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,24 @@
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import { ActiveLink } from "../ActiveLink";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
-export function Header(){
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="logo"/>
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo" />
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
 
-                <SignInButton/>
-            </div>
-        </header>
-    );
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
