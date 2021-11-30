db423b68004e02f555f830f51c02d4c52cf5227d
thyago608 <thyagoribeiro608@gmail.com>
2021-11-30 17:58:14 -0400

    feature getPosts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
 import styles from "./styles.module.scss";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+type PostProps = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -9,28 +24,53 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Updated lib from Styles-Components</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-              aspernatur impedit, consequuntur quod in iste necessitatibus
-              voluptates tenetur voluptatem distinctio animi! Quasi, quos
-              incidunt facilis recusandae ut praesentium necessitatibus laborum.
-            </p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Updated lib from Styles-Components</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-              aspernatur impedit, consequuntur quod in iste necessitatibus
-              voluptates tenetur voluptatem distinctio animi! Quasi, quos
-              incidunt facilis recusandae ut praesentium necessitatibus laborum.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href={post.slug} key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  //Criando um cliente Prismic
+  const prismic = getPrismicClient();
+
+  //Pegando todos os documentos do tipo post do Prismic
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
