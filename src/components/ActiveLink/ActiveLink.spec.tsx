c1c539c6659e9b63e1cb3d52fc5891fc1180dfbd
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-09-10 14:13:32 -0400

    Created Test ActiveLink

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,35 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+describe("Component ActiveLink", () => {
+  it("should render correctly", () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+  });
+
+  it("should be possible to receive the active class", () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByText("Home")).toHaveClass("active");
+  });
+});
